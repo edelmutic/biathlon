e3c1c427f58100c6b2f6a9fb21e12b653c3d164e
edelmutic <edelmutic@gmail.com>
2021-03-25 11:57:55 +0200

    first

---
+++ src/components/columns.js
@@ -0,0 +1,22 @@
+export const COLUMNS = [
+  {
+    Header: 'Final place',
+    accessor: 'final_place',
+  },
+  {
+    Header: 'Name',
+    accessor: 'name',
+  },
+  {
+    Header: 'Country',
+    accessor: 'country',
+  },
+  {
+    Header: 'Shooting speed, sec',
+    accessor: 'speed_of_shooting',
+  },
+  {
+    Header: 'Shooting stats, %',
+    accessor: 'shooting_stats',
+  },
+];
